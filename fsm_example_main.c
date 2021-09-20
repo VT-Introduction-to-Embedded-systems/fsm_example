88d434a440180da8fd530a925b3b53bd3c04889a
Leyla Nazhandali <leyla@vt.edu>
2021-09-20 12:16:55 -0400

    some changes

--- fsm_example_main.c
+++ fsm_example_main.c
@@ -1,9 +1,15 @@
-// This is the code that is related to the example described in the below link:
-// https://sites.google.com/vt.edu/introduction-to-embeddedsystem/finite-state-machines/detailed-example
-
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <stdio.h>
 
-typedef enum {SX, S2, S25, S253, S2534} passcode_state_t;
+typedef enum {SX, S1R, S2R, S3R, S4R, S1W, S2W, S3W, S4W} passcode_state_t;
+
+#define INPUT_ARRAY_SIZE 15
+#define PIN1st_DIGIT 9
+#define PIN2nd_DIGIT 8
+#define PIN3rd_DIGIT 7
+#define PIN4th_DIGIT 6
+
+
 
 bool passcodeFSM(int guess);
 int getGuessInput();
@@ -12,15 +18,17 @@ void openLock();
 void closeLock();
 
 int main(void) {
+    printf ("FSM simulation of the lock with a 4-digit pin:\n");
+
     while (1) {
         // the getGuessInput() function will halt the program
         // until an input is received
         int input = getGuessInput();
 
         // passcodeFSM() is the function containing the FSM
-        bool isUnlocked = passcodeFSM(input);
+        bool unlock = passcodeFSM(input);
 
-        if (isUnlocked)
+        if (unlock)
             playUnlockJingle();
     }
 }
@@ -29,11 +37,13 @@ int getGuessInput()
 {
     // since we have not written any code that reads input from a keypad (we even don't have a keypad on this kit)
     // we use a synthetic input. In other words, we make up some input to test our fsm
-    int guesses[10] = {1, 2, 5, 3, 4, 6, 2, 5, 6, 1};
+    int guesses[INPUT_ARRAY_SIZE] = {9, 8, 7, 6, 0,
+                                     9, 8, 7, 5, 0,
+                                     1, 8, 7, 6, 0};
 
     // using a static variable helps us to retain i so that we get a new input every time we return
     static int i = 0;
-    int guess = guesses[i];
+    int guess = guesses[i%INPUT_ARRAY_SIZE];
     i++;
     return (guess);
 }
@@ -44,49 +54,73 @@ void playUnlockJingle() {
 bool passcodeFSM(int guess) {
     static passcode_state_t currentState = SX;
 
-    bool unlocked = false;
+    bool unlock = false;
+
+    printf ("current state is %d, input is %d", currentState, guess);
 
     switch (currentState) {
-        case SX:
-            if (guess == 2)
-                currentState = S2;
-            break;
+    case SX:
+        if (guess == PIN1st_DIGIT)
+            currentState = S1R;
+        else
+            currentState = S1W;
+        break;
 
-        case S2:
-            if (guess == 5)
-                currentState = S25;
-            else
-                currentState = SX;
-            break;
 
-        case S25:
-            if (guess == 3)
-                currentState = S253;
-            else
-                currentState = SX;
-            break;
+    case S1R:
+        if (guess == PIN2nd_DIGIT)
+            currentState = S2R;
+        else
+            currentState = S2W;
+        break;
 
-        case S253:
-            if (guess == 4)
-            {
-                currentState = S2534;
-                unlocked = true;
-            }
-            else
-                currentState = SX;
-            break;
+    case S2R:
+        if (guess == PIN3rd_DIGIT)
+            currentState = S3R;
+        else
+            currentState = S3W;
+        break;
 
-        case S2534:
-            currentState = SX;
-            break;
+    case S3R:
+        if (guess == PIN4th_DIGIT)
+        {
+            currentState = S4R;
+            unlock = true;
+        }
+        else
+            currentState = S4W;
+        break;
+
+    case S4R:
+        currentState = SX;
+        break;
+
+    case S1W:
+        currentState = S2W;
+        break;
+
+    case S2W:
+        currentState = S3W;
+        break;
+
+    case S3W:
+        currentState = S4W;
+        break;
+
+    case S4W:
+        currentState = SX;
+        break;
+
     }
 
-    if (unlocked)
+    printf (", next state is %d, output is %d\n", currentState, unlock);
+
+    if (unlock)
         openLock();
     else
         closeLock();
 
-    return unlocked;
+    return unlock;
 }
 
 void openLock()
